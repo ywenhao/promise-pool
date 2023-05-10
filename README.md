3e2e624266691e231ce4233106c06903e400cfc3
ywenhao <37856060+ywenhao@users.noreply.github.com>
2023-05-10 10:54:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# promise-pool
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-lynk63)
---
+++ src/pool.ts
@@ -0,0 +1,46 @@
+type IPromise = Promise<boolean>;
+
+export async function pool(promiseList: (() => IPromise)[], limit: number) {
+  const poolSet = new Set<IPromise>();
+
+  let i = 0;
+  for (const promiseFn of promiseList) {
+    if (poolSet.size >= limit) {
+      await Promise.race(poolSet).catch((e) => e);
+    }
+
+    const promise = promiseFn();
+
+    const cb = (res: any) => {
+      poolSet.delete(promise);
+      console.log(i, 'cb', res);
+    };
+
+    const cb2 = (res: any) => {
+      poolSet.delete(promise);
+      console.log(i, 'cb2', res);
+    };
+
+    promise.then(cb).catch(cb2);
+    poolSet.add(promise);
+    i++;
+  }
+}
+
+let a = false;
+const mockRequest = () =>
+  new Promise<boolean>((resolve, reject) => {
+    fetch(
+      'https://console-mock.apipost.cn/mock/f233ab29-8e89-4f8d-ab06-c04e42cea621/mock/f233ab29-8e89-4f8d-ab06-c04e42cea621/1'
+    ).finally(() => {
+      setTimeout(() => {
+        a && resolve(a);
+        !a && reject(new Error('error'));
+      }, 3000);
+      a = !a;
+    });
+  });
+
+const promiseList = Array.from({ length: 20 }).map(() => () => mockRequest());
+
+pool(promiseList, 3);
